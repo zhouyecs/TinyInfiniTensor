d58565dd417275d54f3e33148d0ae6ea6e08a9c7
Zhou Ye <zhouye@stu.pku.edu.cn>
2025-01-16 23:34:44 +0800

    task7

--- src/operators/matmul.cc
+++ src/operators/matmul.cc
@@ -1,4 +1,5 @@
 #include "operators/matmul.h"
+#include "utils/operator_utils.h"
 
 namespace infini
 {
@@ -27,7 +28,25 @@ namespace infini
         // TODO：返回经过 matmul 操作后的 shape
         // REF: https://github.com/onnx/onnx/blob/main/docs/Operators.md#gemm
         // =================================== 作业 ===================================
-        return std::nullopt;
+
+        auto shape_A = inputs[0]->getDims();
+        auto shape_B = inputs[1]->getDims();
+        auto rank_A = shape_A.size();
+        auto rank_B = shape_B.size();
+
+        if (transA && rank_A >= 2) {
+            std::swap(shape_A[rank_A - 1], shape_A[rank_A - 2]);
+        }
+        if (transB && rank_B >= 2) {
+            std::swap(shape_B[rank_B - 1], shape_B[rank_B - 2]);
+        }
+
+        shape_A[rank_A - 1] = 1;
+        shape_B[rank_B - 2] = 1;
+
+        auto shape_output = infer_broadcast(shape_A, shape_B);
+
+        return { {shape_output} };
     }
 
 } // namespace infini
